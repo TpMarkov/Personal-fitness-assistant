990a729d6858e719d31ca665a7ae6ea226c3bb4f
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-07-15 16:03:51 +0300

    add some small changes to generate-program page

--- src/app/generate-program/page.tsx
+++ src/app/generate-program/page.tsx
@@ -6,9 +6,10 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useRef } from "react";
 import { vapi } from "../../lib/vapi";
+import { Card } from "@/components/ui/card";
 
-const ProfilePage = () => {
-  const [calActive, setCallActive] = useState(false);
+const GenerateProgramPage = () => {
+  const [callActive, setCallActive] = useState(false);
   const [connecting, setConnecting] = useState(false);
   const [isSpeaking, setIsSpeaking] = useState(false);
   const [messages, setMessages] = useState([]);
@@ -91,7 +92,57 @@ const ProfilePage = () => {
     };
   }, []);
 
-  return <div>Generating program</div>;
+  const togleCall = async () => {
+    if (callActive) vapi.stop();
+    else {
+      try {
+        setConnecting(true);
+        setMessages([]);
+        setCallEnded(false);
+
+        const fullName = user?.firstName
+          ? `${user.firstName} ${user.lastName || ""}`.trim()
+          : "There";
+
+        await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+          variableValues: {
+            full_name: fullName,
+            //  TODO: send user_id as well later
+          },
+        });
+      } catch (error) {
+        console.log("Failed to start call", error);
+        setConnecting(false);
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen text-foreground overflow-hidden pb-6 pt-24">
+      <div className="container mx-auto px-4 h-full max-w-5xl">
+        {/* HEADER SECTION */}
+        <div className="text-center mb-8">
+          <h1 className="text-3xl font-bold font-mono">
+            <span>Generate Your </span>
+            <span className="text-primary uppercase">Fitness Program</span>
+          </h1>
+          <p className="text-muted-foreground mt-2">
+            Have a voice conversation with our AI assistant to create your
+            personalized plan
+          </p>
+        </div>
+
+        {/* VIDEO CALL AREA */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-8">
+          {/* AI ASSISTANT CARD */}
+          <Card className="bg-card/90 backdrop-blur-sm border border-border overflow-hidden relative"></Card>
+
+          {/* USER CARD */}
+          <Card></Card>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default ProfilePage;
+export default GenerateProgramPage;
